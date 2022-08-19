437bddf488a96aa920651ddfdb4dea96932b3a73
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-08-19 15:57:12 +0430

    fixed bugs

--- assets/base/js/scripts.js
+++ assets/base/js/scripts.js
@@ -1,8 +1,16 @@
 // Variables
 const toggleBtns = document.querySelectorAll('.dv-toggle-btn');
+const nextSlideBtns = document.querySelectorAll('.dv-next-slide-btn');
+const PrevSlideBtns = document.querySelectorAll('.dv-prev-slide-btn');
 
 // Initializing Main Swiper Of Pages With MouseWheel Option
 const MainSwiper = new Swiper('.main-swiper', {mousewheel: true,})
+
+// Adding Event Listener On Each Next Slide Button That Listens To Click And Slides Main Swiper To Next Slide Of Current slide
+nextSlideBtns.forEach(item => item.addEventListener('click', () => MainSwiper.slideNext()))
+
+// Adding Event Listener On Each Previous Slide Button That Listens To Click And Slides Main Swiper To Previous Slide Of Current slide
+PrevSlideBtns.forEach(item => item.addEventListener('click', () => MainSwiper.slidePrev()))
 
 // Adding Event Listener On Each Button That Has Class Of 'dv-toggle-btn' That Listens To Click On Them
 // And Then Gets Value Of Attribute Of 'data-toggle-target' And Saves It In Variables Called 'target'. Then
