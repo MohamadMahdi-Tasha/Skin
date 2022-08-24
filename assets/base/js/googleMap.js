3e920a2c61c3387cf363e4fc8739bd39bd8050cd
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-08-24 15:55:46 +0430

    replaced .dv-about-us-map-slide element with ^<iframe^> tag (google map)

--- assets/base/js/googleMap.js
+++
@@ -1,6 +0,0 @@
-// Variables
-const mapElement = document.getElementById("dv-about-us-map-slide");
-const mapConfig = {center: new google.maps.LatLng(51.508742,-0.120850), zoom:5,}
-
-// A Function That Returns New Google Map And Shows It In 'mapElement' Element With Configs Of 'mapConfig' Variable
-const map = () => new google.maps.Map(mapElement ,mapConfig);
